489584b43b036f1ac1c13927259ea5117250bfd6
Blake Embrey <hello@blakeembrey.com>
2015-03-29 16:05:54 -0300

    update to standard style

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /* global describe, it */
-var assert    = require('assert');
-var lowerCase = require('./');
+var assert = require('assert')
+var lowerCase = require('./')
 
 describe('upper case first', function () {
   it('should upper case the first character a string', function () {
-    assert.equal(lowerCase(null), '');
-    assert.equal(lowerCase('test'), 'Test');
-    assert.equal(lowerCase('TEST'), 'TEST');
-  });
-});
+    assert.equal(lowerCase(null), '')
+    assert.equal(lowerCase('test'), 'Test')
+    assert.equal(lowerCase('TEST'), 'TEST')
+  })
+})
