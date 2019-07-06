ed78eb9411b6323f67d512a460760f3b985326b0
Spencer Corwin <spencercorwin@mac.com>
2019-07-06 13:50:12 -0700

    Clarify function used in upper-case-first tests

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /* global describe, it */
 var assert = require('assert')
-var lowerCase = require('./')
+var upperCaseFirst = require('./')
 
 describe('upper case first', function () {
   it('should upper case the first character a string', function () {
-    assert.equal(lowerCase(null), '')
-    assert.equal(lowerCase('test'), 'Test')
-    assert.equal(lowerCase('TEST'), 'TEST')
+    assert.equal(upperCaseFirst(null), '')
+    assert.equal(upperCaseFirst('test'), 'Test')
+    assert.equal(upperCaseFirst('TEST'), 'TEST')
   })
 })
